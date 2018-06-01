4a95b17521b3dfd2f4a97dbcdb0d6b59740955b9
sina <sinamirshafiee@me.com>
2018-06-01 20:55:42 +0430

    Initial Commit

---
+++ iMesh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMesh
+//
+//  Created by Sina Mirshafiei on 3/11/1397 AP.
+//  Copyright © 1397 AP IAUCTB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
